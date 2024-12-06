ef1058c3a1a8e3fefa2bdb3ef6e7f02b5b185f60
watashiwasta <aidanisop69@gmail.com>
2024-12-07 06:50:04 +1100

    Updated subpath with new changes

--- js/script.js
+++ js/script.js
@@ -36,7 +36,7 @@ backgroundAudio.volume = 0.1;
 let currentMarketCap = 0;
 let od;
 let updateInterval;
-let currentAddress = 'TO BE UPDATED'; // Set this to your desired address, empty for pending
+let currentAddress = 'kWemdf68DgzZRgwMiSJqTKDHPTUKwTF9WQ6nAL8pump'; // Set this to your desired address, empty for pending
 
 // Create cursor
 const cursor = document.createElement('span');
